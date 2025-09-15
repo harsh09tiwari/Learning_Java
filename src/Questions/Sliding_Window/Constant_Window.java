8943fed85175c5e115ca96616c8fc656e02c7d21
Harsh <139758128+harsh09tiwari@users.noreply.github.com>
2025-09-16 02:07:22 +0530

    updated

--- src/Questions/Sliding_Window/Constant_Window.java
+++ src/Questions/Sliding_Window/Constant_Window.java
@@ -1,6 +1,6 @@
-package Questions;
+package Questions.Sliding_Window;
 
-public class SlidingWindow {
+public class Constant_Window {
 
     public static void main(String[] args){
         int[] arr = {1,3,-1,10,5,3,6,7};
